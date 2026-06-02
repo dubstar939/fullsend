fa13cd028a589ee868c81efdbb63123194fa1836
dubstar939 <cwuthnow@gmail.com>
2026-06-02 14:38:15 -0500

    Update StartMenu.tsx

--- src/components/StartMenu.tsx
+++ src/components/StartMenu.tsx
@@ -1,1 +1,18 @@
-
+import startMenuBg from "../art/fullsendstartmenu.png";
+<div
+  className="start-menu"
+  style={{
+    backgroundImage: `url(${startMenuBg})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    width: "100vw",
+    height: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  }}
+>
+  {/* Your title, buttons, etc */}
+</div>
